3dcf65a96a4dd2bf13546489a44f5430b23f94f6
zavaultraz <zavazaneta@gmail.com>
2024-02-28 10:58:52 +0700

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
 @extends('home.includes.parent');
 @section('content')
-    .container
+    <div class="container">
+        <div class="row">
+            <h1>Wellcome {{ Auth::user()->name }}</h1>
+        </div>
+    </div>
 @endsection
