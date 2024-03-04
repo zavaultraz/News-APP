30ccc311c12bea9517e1f51b3544929af8a25910
zavaultraz <zavazaneta@gmail.com>
2024-03-04 15:01:23 +0700

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,10 @@
 
     <div class="container card p-4">
         <div class="row">
-            <h1>Wellcome {{ Auth::user()->name }}</h1>
+            <h1>Wellcome {{ Auth::user()->name }} <i class="bi bi-emoji-wink"></i></h1>
             <hr>
             <div class="p-2">
-                <h3 class="text-center p-2 text-uppercase "><strong>{{Auth::user()->name}} INFO</strong></h3>
+                <h3 class="text-center p-2 text-uppercase "><strong>{{Auth::user()->name}} INFO <i class="bi bi-person-circle"></i></strong></h3>
             <ul class="list-group">
                 <li class="list-group-item" aria-current="true">Username : <strong>{{Auth::user()->name}}</strong></li>
                 <li class="list-group-item">Email : <strong>{{Auth::user()->email}}</strong> </li>
