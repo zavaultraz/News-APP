e955622d7224f3ed7463ad3c0fd484d527d5af40
zavaultraz <zavazaneta@gmail.com>
2024-03-21 14:29:02 +0700

    yas

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use Exception;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            $this->validate($request, [
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            // cek credentials (login)
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt([
+                'email' => $credentials['email'],
+                'password' => $credentials['password']
+            ])) {
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized'
+                ], 'Authentication Failed', 500);
+            };
+
+            // cek klo password g sesuai
+            $user = User::where('email', $credentials['email'])->first();
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Invalid Credentials');
+            };
+
+            // jika berasil cek password maka loginkan
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'bearer',
+                'user' => $user
+            ], 'Authenticated', 200);
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
+            ], 'Authentication Failed', 500);
+        }
+    }
+    public function register(Request $request)
+    {
+        try {
+            $this->validate($request, [
+                'name' => 'required|string|max:100',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required',
+                'confirm_password' => 'required'
+            ]);
+            //cek kondisi password 
+            if ($request->password != $request->confirm_password) {
+                return ResponseFormatter::error([
+                    'message' => 'password not match'
+                ], 'Authentication Failed', 401);
+            }
+
+            //create akun
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+            //get data akunn\
+            $user = User::where('email', $request->email)->first();
+            //create token
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            //response 
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Authenticed', 200);
+        } catch (\Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
+            ], 'Authentication Failed', 500);
+        }
+    }
+    public  function logout(Request $request)
+    {
+        $token = $request->user()->currentAccessToken()->delete();
+        return ResponseFormatter::success($token , 'Token Revoked');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Route9s
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware'=>['auth:sanctum']], function(){
+    Route::post('/logout', [\App\Http\Controllers\API\AuthController::class, 'logout']);
+});
+
+
+Route::post('/login',[\App\Http\Controllers\API\AuthController::class,'login']);
+
+Route::post('/register',[\App\Http\Controllers\API\AuthController::class,'register']);
+
