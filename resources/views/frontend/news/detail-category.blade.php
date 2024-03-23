5358eacb7f688b85acb07839223759bff5df8588
zavaultraz <zavazaneta@gmail.com>
2024-03-23 09:44:03 +0700

    up

--- resources/views/frontend/news/detail-category.blade.php
+++ resources/views/frontend/news/detail-category.blade.php
@@ -20,7 +20,7 @@
                             <div class="d-flex align-items-center author">
                                 <div class="photo"><img src="assets/img/person-2.jpg" alt="" class="img-fluid"></div>
                                 <div class="name">
-                                    <h3 class="m-0 p-0">zavv</h3>
+                                    <h3 class="m-0 p-0">zavvv</h3>
                                 </div>
                             </div>
                         </div>
